f9a8334815d35c7abea2f22d771971b41113bb73
Myomyintaung <myomyintaung1411@gmail.com>
2021-11-14 20:38:20 +0800

    deploy

---
+++ precache-manifest.9b9cdf43fe19618fb249119f6ea2088e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b80c676efc365839f6466c8dab5167",
+    "url": "/pwatest/index.html"
+  },
+  {
+    "revision": "769e0f1ec1ca1461d0f3a0fcea4b33d6",
+    "url": "/pwatest/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwatest/robots.txt"
+  },
+  {
+    "revision": "0c9f34b4140d71ab1fc3",
+    "url": "/pwatest/static/css/about.abf8fee5.css"
+  },
+  {
+    "revision": "483c949cbc9ecac89d45",
+    "url": "/pwatest/static/css/app.fe071308.css"
+  },
+  {
+    "revision": "9398728d36b9b597972b",
+    "url": "/pwatest/static/css/chunk-vendors.d81bd57c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwatest/static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8e9addfca9cce6760fdd418a775dd981",
+    "url": "/pwatest/static/img/sharereply.8e9addfc.png"
+  },
+  {
+    "revision": "00f1da3c454e916119317562d803ad9c",
+    "url": "/pwatest/static/img/tiktokheart.00f1da3c.png"
+  },
+  {
+    "revision": "6f1a7a5021e14b67d553bb2e0b23e213",
+    "url": "/pwatest/static/img/titkokfullheart.6f1a7a50.png"
+  },
+  {
+    "revision": "0c9f34b4140d71ab1fc3",
+    "url": "/pwatest/static/js/about.9cd813c4.js"
+  },
+  {
+    "revision": "483c949cbc9ecac89d45",
+    "url": "/pwatest/static/js/app.2a27201b.js"
+  },
+  {
+    "revision": "9398728d36b9b597972b",
+    "url": "/pwatest/static/js/chunk-vendors.5e23dc75.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,32 @@
+importScripts("/pwatest/precache-manifest.9b9cdf43fe19618fb249119f6ea2088e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
+/* eslint-disable */
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(fetch(event.request));
+// });
+
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.suppressWarnings();
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+// // install new service worker when ok, then reload page.
+// self.addEventListener("message", msg=>{
+//     if (msg.data.action=='skipWaiting'){
+//         self.skipWaiting()
+//     }
+// })
+
+
+console.log('Started', self);
+self.addEventListener('install', function (event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function (event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function (event) {
+  console.log('Push message received', event);
+});
+
